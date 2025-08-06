06442df18e6cdfb279ab2f54d58cf6877dc757d7
David <40246197+bumothekid@users.noreply.github.com>
2025-08-06 17:24:11 +0200

    Refactor for simlified singleton instances

--- main.py
+++ main.py
@@ -8,14 +8,14 @@ from app.utils.exceptions import (
     ConflictError,
     PermissionError
 )
-from app.utils.authentication_managment import AuthenticationManager
-from app.utils.user_managment import UserManagment
-from app.utils.clothing_managment import ClothingManager
-from app.utils.outfit_managment import OutfitManager
+from app.utils.authentication_managment import authentication_manager
+from app.utils.user_managment import user_manager
+from app.utils.clothing_managment import clothing_manager
+from app.utils.outfit_managment import outfit_manager
 from app.main.routes import api as main
 from app.auth.routes import auth
-from app.users.routes import users
-from app.uploads.routes import uploads
+#from app.users.routes import users
+#from app.uploads.routes import uploads
 from app.clothing.routes import clothing
 from app.images.routes import images
 from app.outfit.routes import outfit
@@ -27,9 +27,9 @@ logger = get_logger()
 limiter.init_app(api)
 api.register_blueprint(main)
 api.register_blueprint(auth, url_prefix="/auth")
-api.register_blueprint(users, url_prefix="/users")
+#api.register_blueprint(users, url_prefix="/users")
 api.register_blueprint(clothing, url_prefix="/clothing")
-api.register_blueprint(uploads, url_prefix="/uploads")
+#api.register_blueprint(uploads, url_prefix="/uploads")
 api.register_blueprint(images, url_prefix="/images")
 api.register_blueprint(outfit, url_prefix="/outfit")
 logger.info("API registered blueprints")
@@ -61,14 +61,9 @@ def internal_error_handler(error):
 def not_found_error_handler(error):
     return jsonify({"error": "Resource not found."}), 404
 
-UserManagment.getInstance()._checkTable()
-AuthenticationManager.getInstance()._checkTable()
-ClothingManager.getInstance()._checkTable()
-OutfitManager.getInstance()._checkTable()
-
 if not os.path.exists("logs"):
     try:
-        os.makedirs("logs")
+        os.makedirs("logs", exist_ok=True)
     except OSError as e:
         logger.error(f"Error creating logs directory: {e}")
         
@@ -76,15 +71,24 @@ if not os.path.exists("logs"):
 
 if not os.path.exists("app/static/temp"):
     try:
-        os.makedirs("app/static/temp")
+        os.makedirs("app/static/temp", exist_ok=True)
     except OSError as e:
         logger.error(f"Error creating temp directory: {e}")
         
     logger.info("Temporary images directory created or already exists.")
 
+def initialize_database():
+    user_manager.ensure_table_exists()
+    authentication_manager.ensure_table_exists()
+    clothing_manager.ensure_table_exists()
+    outfit_manager.ensure_table_exists()
+    
+    logger.info("Database initialized successfully.")
 
 if __name__ == '__main__':
+    initialize_database()
     logger.info("Starting API in debug mode")
     api.run(host="0.0.0.0", debug=True, port=8000)
 else:
+    initialize_database()
     logger.info("Starting API in production mode")
