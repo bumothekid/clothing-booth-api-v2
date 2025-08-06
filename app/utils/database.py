eeaef5048b9fbf5aeb6f817e45454219ca942a83
David <40246197+bumothekid@users.noreply.github.com>
2025-08-07 01:14:34 +0200

    Add database host and port to configuration env and check if connection is working

--- app/utils/database.py
+++ app/utils/database.py
@@ -1,6 +1,9 @@
+from sys import exit
 from mysql.connector import pooling, MySQLConnection
-from dotenv import load_dotenv
+from app.utils.logging import get_logger
 from os import getenv
+
+logger = get_logger()
 
 class Database:
     _pool: pooling.MySQLConnectionPool = None
@@ -8,15 +11,26 @@ class Database:
     @classmethod
     def getConnection(cls) -> MySQLConnection:
         if cls._pool is None:
-            load_dotenv()
             
-            cls._pool = pooling.MySQLConnectionPool(
-                pool_name="connectionPool",
-                pool_size=5,
-                host="localhost",
-                user=getenv("DATABASE_USERNAME"),
-                password=getenv("DATABASE_PASSWORD"),
-                database=getenv("DATABASE")
-            )
             
+            try:
+                cls._pool = pooling.MySQLConnectionPool(
+                    pool_name="connectionPool",
+                    pool_size=5,
+                    host=getenv("DATABASE_HOST", "localhost"),
+                    port=getenv("DATABASE_PORT", "3306"),
+                    user=getenv("DATABASE_USERNAME"),
+                    password=getenv("DATABASE_PASSWORD"),
+                    database=getenv("DATABASE_NAME")
+                )
+                
+                connection = cls._pool.get_connection()
+                connection.ping()
+                connection.close()
+                
+                logger.debug(f"Successfully created database connection pool.")
+            except Exception as e:
+                logger.critical(f"Failed to create database connection pool: {e}")
+                exit(1)
+
         return cls._pool.get_connection()
