f616be651308aa0ceb7855bea691b8126ce99d18
David <40246197+bumothekid@users.noreply.github.com>
2025-08-09 11:03:49 +0200

    Add route to get openapi.yaml file for future api documentation

--- app/uploads/routes.py
+++ app/uploads/routes.py
@@ -1,34 +1,31 @@
 from flask import Blueprint, jsonify, send_from_directory
 from app.utils.limiter import limiter
 from app.utils.logging import get_logger
-from werkzeug.exceptions import NotFound
-from app.utils.user_managment import user_manager
-from app.utils.exceptions import UserNotFoundError
 
 uploads = Blueprint("uploads", __name__)
 logger = get_logger()
 
-@uploads.route('/profile_pictures/default/<filename>.png', methods=['GET'])
-@uploads.route('/profile_pictures/<user_id>.webp', methods=['GET'])
-@uploads.route('/profile_pictures/<user_id>', methods=['GET'])
-@limiter.limit("3 per minute")
-def getProfilePicture(user_id=None, filename=None):
-    try:
-        if filename:
-            return send_from_directory('app/static/profile_pictures/default', f'{filename}.png')
-        
-        return send_from_directory('app/static/profile_pictures', f'{user_id}.webp')
-    except NotFound:
-        try:
-            return send_from_directory(user_manager.getUserProfilePicture(user_id))
-        except UserNotFoundError as e:
-            return send_from_directory('app/static/profile_pictures/default', f'{user_id}.png')
-        except Exception as e:
-            logger.error(f"An unexpected error occurred: {e}")
-            return jsonify({"error": f"An unexpected error occurred: {e}"}), 500
-    except Exception as e:
-        logger.error(f"An unexpected error occurred: {e}")
-        return jsonify({"error": f"An unexpected error occurred: {e}"}), 500
+#@uploads.route('/profile_pictures/default/<filename>.png', methods=['GET'])
+#@uploads.route('/profile_pictures/<user_id>.webp', methods=['GET'])
+#@uploads.route('/profile_pictures/<user_id>', methods=['GET'])
+#@limiter.limit("3 per minute")
+#def getProfilePicture(user_id=None, filename=None):
+#    try:
+#        if filename:
+#            return send_from_directory('app/static/profile_pictures/default', f'{filename}.png')
+#        
+#        return send_from_directory('app/static/profile_pictures', f'{user_id}.webp')
+#    except NotFound:
+#        try:
+#            return send_from_directory(user_manager.getUserProfilePicture(user_id))
+#        except UserNotFoundError as e:
+#            return send_from_directory('app/static/profile_pictures/default', f'{user_id}.png')
+#        except Exception as e:
+#            logger.error(f"An unexpected error occurred: {e}")
+#            return jsonify({"error": f"An unexpected error occurred: {e}"}), 500
+#    except Exception as e:
+#        logger.error(f"An unexpected error occurred: {e}")
+#        return jsonify({"error": f"An unexpected error occurred: {e}"}), 500
 
 @uploads.route('/clothing_images/<clothing_id>.webp', methods=['GET'])
 @uploads.route('/clothing_images/<clothing_id>', methods=['GET'])
@@ -36,8 +33,6 @@ def getProfilePicture(user_id=None, filename=None):
 def getClothingImage(clothing_id):
     try:
         return send_from_directory('app/static/clothing_images', f'{clothing_id}.webp')
-    except NotFound:
-        return jsonify({"error": "Clothing image not found"}), 404
     except Exception as e:
         logger.error(f"An unexpected error occurred: {e}")
         return jsonify({"error": f"An unexpected error occurred: {e}"}), 500
@@ -52,8 +47,10 @@ def getTempImage(filename):
 
     try:
         return send_from_directory('app/static/temp', f'{filename}')
-    except NotFound:
-        return jsonify({"error": "Temporary image not found"}), 404
     except Exception as e:
         logger.error(f"An unexpected error occurred: {e}")
         return jsonify({"error": f"An unexpected error occurred: {e}"}), 500
+
+@uploads.get('/openapi.yaml')
+def get_openapi_spec():
+    return send_from_directory('app/static', 'openapi.yaml')
