c42528dd307841731f71d8a413a6c4975fdec4cb
David <40246197+bumothekid@users.noreply.github.com>
2025-08-10 00:13:11 +0200

    Refactor guest registration and login methods to use authentication manager for improved functionality

--- app/auth/routes.py
+++ app/auth/routes.py
@@ -10,13 +10,13 @@ auth = Blueprint("auth", __name__)
 @auth.route('/guest', methods=['POST'])
 @limiter.limit('5 per hour')
 def register_guest():
-    access_token, expires_in, refresh_token = user_manager.register_guest()
+    access_token, expires_in, refresh_token = authentication_manager.register_guest()
 
     return jsonify({"access_token": access_token, "expires_in": expires_in, "refresh_token": refresh_token}), 201
 
 @auth.route('/refresh', methods=['POST'])
 @limiter.limit('5 per minute')
-def refresh_token():
+def refresh_access_token():
     data = request.get_json()
     refresh_token = data.get("refresh_token")
     access_token = data.get("access_token")
@@ -51,15 +51,19 @@ def upgrade_guest():
     user = user_manager.upgrade_guest_account(token, email, username, password, profile_picture)
     
     return jsonify(user.from_dict), 201
-
-"""
-# upgrade account from guest to user
-@auth.route('/upgrade', methods=['POST'])
-@limiter.limit('5 per')
-@authorize_request
-def upgrade_guest():
-    return
-"""
+    
+@auth.route("/login", methods=["POST"])
+@limiter.limit("5 per minute")
+def login():
+    data: dict = request.get_json()
+    
+    email = data.get("email", None)
+    username = data.get("username", None)
+    password = data.get("password", None)
+    
+    access_token, expires_in, refresh_token = authentication_manager.sign_in_user(email, username, password)
+    
+    return jsonify({"access_token": access_token, "expires_in": expires_in, "refresh_token": refresh_token})
 
 """
 @auth.route('/login', methods=['POST'])
