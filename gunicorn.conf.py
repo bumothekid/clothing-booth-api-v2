b910dccd16530cfa6e0052aa7b4e6796323fd050
David <40246197+bumothekid@users.noreply.github.com>
2025-08-16 11:57:27 +0200

    Set preload_app to False to prevent artifacts during background removal

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -3,7 +3,7 @@ import multiprocessing
 
 os.makedirs("logs", exist_ok=True)
 
-preload_app = True
+preload_app = False # set to false because causes artifacts on background removal
 bind = "0.0.0.0:8000"
 workers = 1 #multiprocessing.cpu_count() * 2 + 1
 errorlog = os.path.join(os.path.dirname(__file__), "logs", "gunicorn_error.log")
