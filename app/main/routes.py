0719c82757d2ac4583dc6c61fcb3976444a126e3
David <40246197+bumothekid@users.noreply.github.com>
2026-02-22 18:33:39 +0100

    Add ping route for apps to check api availability

--- app/main/routes.py
+++ app/main/routes.py
@@ -1,7 +1,7 @@
 from flask import Blueprint
 
-# ? Add 'https://clothing-booth.com' before profile picture URL // for production
-# ? Go through the code and add comments
-# ? Go through all the routes and check their response codes
-
 api = Blueprint("main", __name__)
+
+@api.route('/ping', methods=['GET'])
+def ping_reachablility():
+    return {}, 200
